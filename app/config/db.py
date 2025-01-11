2f04201662aeaa66a70eb419e5c7e6dde191d8dd
Ivan Palencia <ivanpalenciab@gmail.com>
2025-01-11 12:52:20 -0500

    Switch database from test to actual database.

--- app/config/db.py
+++ app/config/db.py
@@ -10,6 +10,8 @@ load_dotenv()
 
 password = os.getenv("DATABASE_PASSWORD")
 host = os.getenv("DATABASE_HOST")
+database_name = os.getenv("DATABASE_NAME")
+database_user = os.getenv("DATABASE_USER")
 
-engine = create_engine(f"postgresql://postgres:{password}@{host}:5434/prueba")
+engine = create_engine(f"postgresql://{database_user}:{password}@{host}:5434/{database_name}")
 conn = engine.connect()
